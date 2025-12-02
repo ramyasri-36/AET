a6cc5052953eda2b6f6701c9dff1c1ed072c4771
ramyasri-36 <ramyasri.papolu@slu.edu>
2025-12-02 19:22:06 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -14,7 +14,7 @@ from datetime import datetime, timezone
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
-# MongoDB connection
+# MongoDB connection set
 mongo_url = os.environ['MONGO_URL']
 client = AsyncIOMotorClient(mongo_url)
 db = client[os.environ['DB_NAME']]
